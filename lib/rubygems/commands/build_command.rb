9ad4747f3771ff3b36f6ea14ccf394cb3cae3066
Al Chou <Albert.Chou@Cloud9Analytics.com>
2011-07-07 13:22:05 -0700

    Added informational alert pointing user to rake-compiler GitHub repo.

--- lib/rubygems/commands/build_command.rb
+++ lib/rubygems/commands/build_command.rb
@@ -28,29 +28,8 @@ class Gem::Commands::BuildCommand < Gem::Command
 
       if spec then
         if options[:prebuilt_extension]
-          # From outside this class you can instantiate a spec like so:
-          # require 'rubygems/commands/build_command'
-          # build_command = Gem::Commands::BuildCommand.new
-          # spec = build_command.load_gemspec gemspec
-
-          spec.extensions = []
-
-          extension_subdirectories = []
-          Dir.foreach( 'ext' ) { |directory_name|
-            next if /\A\.(\.)?\Z/ =~ directory_name
-
-            spec.require_paths << "ext/#{directory_name}"
-            extension_subdirectories << directory_name
-          }
-
-          extension_subdirectories.each { |extension_subdirectory|
-            next if /\A\.(\.)?\Z/ =~ extension_subdirectory
-
-            directory = 'ext/' + extension_subdirectory
-            Dir.foreach( directory ) { |filename|
-              spec.files << "#{directory}/#{filename}"
-            }
-          }
+          alert 'Use rake-compiler for this functionality.'
+          terminate_interaction
         end
 
         Gem::Builder.new(spec).build
